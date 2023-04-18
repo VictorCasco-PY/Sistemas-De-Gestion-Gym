490aa128e9be5ceb6085a59b7a0b3270f56b6aca
arvidjj <andy.oj14@gmail.com>
2023-04-18 15:08:55 -0400

    Lista CLientes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,14 +3,13 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import RegistroCliente from './pages/RegistroCliente'
-import ListaClientes from './pages/ListaClientes'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      <ListaClientes />
+      <RegistroCliente />
     </div>
   )
 }
