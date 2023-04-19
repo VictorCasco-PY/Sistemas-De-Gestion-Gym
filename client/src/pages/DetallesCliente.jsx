42f4d656a96a7a9342048bee75daedcde90829a8
arvidjj <andy.oj14@gmail.com>
2023-04-19 19:04:19 -0400

    Actualizado estructura de paginas clientes

--- client/src/pages/DetallesCliente.jsx
+++ client/src/pages/DetallesCliente.jsx
@@ -8,18 +8,67 @@ export function DetallesCliente(props) {
         <div>
             <div className="clienteHeader">
                 <h1>PEdro ppp</h1>
-                <div className="headerEstadoPago">
-                    <div className="estadoPago1">
+                <div className="infoBubble">
+                    <div className="bubbleTitle">
                         <p>Estado de Pago</p>
                     </div>
-                    <div className="estadoPago2">
+                    <div className="bubbleInfo">
                         <p>estadopago ppp</p>
                     </div>
                 </div>
             </div>
 
             <div className='pageMain'>
+                <div className="mainClientInfo">
+                    <div className="clientInfoLeft">
+                        <div className="infoBubble">
+                            <div className="bubbleTitle">
+                                <p>Telefono</p>
+                            </div>
+                            <div className="bubbleInfo">
+                                <p>1231234</p>
+                            </div>
+                        </div>
+                        <div className="infoBubble">
+                            <div className="bubbleTitle">
+                                <p>Dirección</p>
+                            </div>
+                            <div className="bubbleInfo">
+                                <p>calle 1 calle 2</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="clientInfoRight">
+                        <div className="infoBubble variant1">
+                            <div className="bubbleTitle">
+                                <p>Plan</p>
+                            </div>
+                            <div className="bubbleInfo">
+                                <p>Diario</p>
+                            </div>
+                        </div>
+                        <div className="infoBubble variant2">
+                            <div className="bubbleTitle">
+                                <p>RUC</p>
+                            </div>
+                            <div className="bubbleInfo">
+                                <p>12341234</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
 
+                <table>
+                    <thead>
+                        <th>Fecha</th>
+                        <th>Peso</th>
+                        <th>Brazos</th>
+                        <th>Piernas</th>
+                    </thead>
+                    <tbody>
+                        MAPEO
+                    </tbody>
+                </table>
             </div>
         </div>
     )
