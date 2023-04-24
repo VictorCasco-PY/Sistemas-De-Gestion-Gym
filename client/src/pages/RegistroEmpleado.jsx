c75f007a0ecc40e2934a54fe0770c0cdf4f8f180
JulioLaneri <juliobenitez@fiuni.edu.py>
2023-04-24 19:35:03 -0400

    Parte de Julio

--- client/src/pages/RegistroEmpleado.jsx
+++ client/src/pages/RegistroEmpleado.jsx
@@ -1,87 +1,148 @@
 import React, { useState, useEffect } from "react";
 
 const RegistroEmpleado = () => {
-    const [empleados, setEmpleados] = useState(
-        {
-            user: "",
-            pass: "",
-            str_nombre: "",
-            time_inicio_trabajo: "",
-            time_fin_trabajo: "",
-            str_telefono: "",
-            str_cedula: "",
-            str_direccion: "",
-        }
-    );
-    const handleChange = (event) => {
-        setEmpleados({
-            ...empleados,
-            [event.target.name]: event.target.value,
-        });
-    };
+  const [empleados, setEmpleados] = useState({
+    user: "",
+    pass: "",
+    str_nombre: "",
+    time_inicio_trabajo: "",
+    time_fin_trabajo: "",
+    str_telefono: "",
+    str_cedula: "",
+    str_direccion: "",
+  });
+  const handleChange = (event) => {
+    setEmpleados({
+      ...empleados,
+      [event.target.name]: event.target.value,
+    });
+  };
 
-    // función para manejar el envío del formulario
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        console.log(empleados);
-    };
+  // función para manejar el envío del formulario
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(empleados);
+  };
 
-    return (
-        <div className="columns is-centered">
-            <div className="column is-three-fifths-desktop">
-                <div className="box">
-                    <form onSubmit={handleSubmit}>
-                        <h2>Registrar Empleado</h2>
-                        <div>
-                            <input className="input" type="email" name="user" value={empleados.user} placeholder="Corre Electronico" onChange={handleChange} />
-                        </div>
-                        <div>
-                            <input className="input mt-2" type="password" name="pass" value={empleados.pass} placeholder="Contraseña" onChange={handleChange} />
-                        </div>
-                            <div className="column is-one-quarter">
-                            < h2>Rol: </h2>
-                            </div>
-                            <div className="field">
-                            <div className="control">
-                        <label className="radio">
-                        <input type="radio" name="question"/>
-                            Cajero
-                        </label>
-                        <label className="radio">
-                        <input type="radio" name="question"/>
-                            Entrenador
-                        </label>
-                        </div>
-                        </div>
-                        <div className="columns">
-                            <div className="column is-three-fifths">
-                                <input className="input" type="text" name="str_nombre" value={empleados.str_nombre} onChange={handleChange} placeholder='Nombres Y Apellidos' />
-                            </div>
-                            <div className="column is-4 is-offset-0">
-                                <input className="input" type="date" name="time_fin_trabajo" value={empleados.time_fin_trabajo} onChange={handleChange} placeholder='Fecha de Nac' />
-                            </div>
-                        </div>
-                        <div className="columns">
-                            <div className="column is-three-fifths">
-                                <input className="input" type="text" name="str_telefono" value={empleados.str_telefono} onChange={handleChange} placeholder='Teléfono' />
-                            </div>
-                            <div className="column">
-                                <input className="input" type="text" name="str_cedula" value={empleados.str_cedula} onChange={handleChange} placeholder='Cédula' />
-                            </div>
-                                </div>
-                                <div>
-                                    <input className="input" type="text" name="str_direccion" value={empleados.str_direccion} onChange={handleChange} placeholder='Dirección' />
-                                </div>
-                                <div className="columns is-mobile">
-                                <div className="column is-4 is-offset-8">
-                                <button className="button is-primary" type='submit'>Registrar</button>
-                                </div>
-                                </div>
-            </form>
+  return (
+    <div className="columns is-centered">
+      <div className="column is-three-fifths-desktop">
+        <div className="box">
+          <form onSubmit={handleSubmit}>
+            <center>
+              <h1 className="mb-2 title is-3 has-text-primary">
+                Registrar Empleado
+              </h1>
+            </center>
+            <div>
+              <input
+                className="input mb-2"
+                type="email"
+                name="user"
+                value={empleados.user}
+                placeholder="Corre Electronico"
+                onChange={handleChange}
+              />
+            </div>
+            <div>
+              <input
+                className="input mt-2"
+                type="password"
+                name="pass"
+                value={empleados.pass}
+                placeholder="Contraseña"
+                onChange={handleChange}
+              />
+            </div>
+                
+              <div className="field">
+                <div className="control">
+                <span>Rol: </span>
+                    <label className="radio">
+                      <input type="radio" name="question" value="cajero"/>
+                      Cajero
+                    </label>
+                    <label className="radio">
+                      <input type="radio" name="question" value="entrenador" />
+                      Entrenador
+                    </label>
+                </div>
+            </div>
+            <div className="columns">
+              <div className="column">
+                <input
+                  className="input"
+                  type="text"
+                  name="str_nombre"
+                  value={empleados.str_nombre}
+                  onChange={handleChange}
+                  placeholder="Nombres Y Apellidos"
+                />
+              </div>
+              <div className="column">
+                <input
+                  className="input"
+                  type="text"
+                  name="str_telefono"
+                  value={empleados.str_telefono}
+                  onChange={handleChange}
+                  placeholder="Teléfono"
+                />
+              </div>
+            </div>
+            <div className="columns">
+              <div className="column">
+                <input
+                  className="input"
+                  type="date"
+                  name="time_inicio_trabajo"
+                  value={empleados.time_inicio_trabajo}
+                  onChange={handleChange}
+                />
+              </div>
+              <div className="column">
+                <input
+                  className="input"
+                  type="date"
+                  name="time_fin_trabajo"
+                  value={empleados.time_fin_trabajo}
+                  onChange={handleChange}
+                />
+              </div>
+            </div>
+            <div className="columns">
+              <div className="column">
+                <input
+                  className="input"
+                  type="text"
+                  name="str_cedula"
+                  value={empleados.str_cedula}
+                  onChange={handleChange}
+                  placeholder="Cédula"
+                />
+              </div>
+              <div className="column">
+                <input
+                  className="input"
+                  type="text"
+                  name="str_direccion"
+                  value={empleados.str_direccion}
+                  onChange={handleChange}
+                  placeholder="Dirección"
+                />
+              </div>
+            </div>
+            <div className="buttons">
+                <button className="ml-auto mt-2 button is-primary is-outlined" type="submit">
+                  Registrar
+                </button>
+
+            </div>
+          </form>
         </div>
-        </div>
-        </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
 export default RegistroEmpleado;
