a9460a3afb5515fd4058390c1795d1bf954d6fdd
VictorCasco-PY <cascoalmada@gmail.com>
2023-04-23 21:33:20 -0400

    Conexion frontend y backend

--- server/server.js
+++ server/server.js
@@ -4,11 +4,18 @@ import bodyParser from "body-parser";
 import planesPagosRouter from "./routers/planes-de-pagos.routes.js";
 import medicionesClientesRoutes from "./routers/mediciones-clientes.routes.js";
 import tipoModalidadPagoRouter from "./routers/tipos-modalidades-de-pagos.routes.js";
+import cors from "cors";
 
 const app = express();
 
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+  })
+);
+
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // parse application/json
 app.use(bodyParser.json());
@@ -16,14 +23,13 @@ app.use(bodyParser.json());
 // Usamos los enrutadores
 app.use(planesPagosRouter);
 app.use(clientesRouter);
-app.use(medicionesClientesRoutes)
-app.use(tipoModalidadPagoRouter)
-
+app.use(medicionesClientesRoutes);
+app.use(tipoModalidadPagoRouter);
 
 app.get("/", async (req, res) => {
-    res.json({
-        title: "GymBRO Web Api",
-    });
+  res.json({
+    title: "GymBRO Web Api",
+  });
 });
 
 app.listen(8000, () => console.log("Escuchando en puerto 8000"));
