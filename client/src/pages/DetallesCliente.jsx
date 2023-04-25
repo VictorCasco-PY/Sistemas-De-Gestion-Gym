751adca419d4c9f3b4b5a743536b082d946c8d41
arvidjj <andy.oj14@gmail.com>
2023-04-25 03:14:32 -0400

    Terminado requests axios tomando en cuenta el url

--- client/src/pages/DetallesCliente.jsx
+++ client/src/pages/DetallesCliente.jsx
@@ -3,41 +3,45 @@ import { useParams } from 'react-router-dom';
 import axios from 'axios';
 
 export function DetallesCliente() {
+    const  id  = useParams().name;
+
     const [cliente, setCliente] = useState(null);
     const [clientePers, setClientePers] = useState(null);
     const [clienteMed, setClienteMed] = useState(null);
 
     useEffect(() => {
-        axios.get('http://localhost:8000/cliente/1/medicion-cliente')
+        axios.get(`http://localhost:8000/cliente/${id}/medicion-cliente`)
             .then(response => setClienteMed(response.data))
             .catch(error => console.log(error));
+            
 
-        axios.get('http://localhost:8000/cliente/1/plan-de-pago')
+        axios.get(`http://localhost:8000/cliente/${id}/plan-de-pago`)
             .then(response => setCliente(response.data))
             .catch(error => console.log(error));
 
-        axios.get('http://localhost:8000/cliente/1')
+        axios.get(`http://localhost:8000/cliente/${id}`)
             .then(response => setClientePers(response.data))
             .catch(error => console.log(error));
+            console.log(id)
     }, []);
 
     if (!cliente || !clientePers ||!clienteMed) {
-        return <div className='title'>Cargando cliente...</div>;
+        return <div className='title'>Cliente no encontrado.</div>;
     }
 
     return (
         <div className='columns is-flex-direction-column is-align-content-center is-multiline is-centered'>
             <div className="column columns is-half headerTitle clienteHeader m-2">
-                <div className='is-flex is-justify-content-center is-align-content-center column headerTitle has-text-centered'>
+                <div className='is-flex is-justify-content-center column headerTitle has-text-centered'>
                     <h1>{cliente.str_nombre_cliente}</h1>
                 </div>
                 <div className="column">
                     <div className="infoBubble">
                         <div className="bubbleTitle has-text-white">
-                            <p className='is-size-5'>Estado de Pago</p>
+                            <p className='is-size-6'>Estado de Pago</p>
                         </div>
                         <div className="bubbleInfo">
-                            <p>{cliente.estado_de_pago}</p>
+                            <p className='is-size-5'>{cliente.estado_de_pago}</p>
                         </div>
                     </div>
                 </div>
@@ -45,7 +49,7 @@ export function DetallesCliente() {
 
             <div className='column is-half is-flex is-flex-direction-column is-justify-content-center p-6 pageMain has-background-light'>
                 <div className="mainClientInfo columns">
-                    <div className="column clientInfoLeft d-flex ">
+                    <div className="clientInfoLeft d-flex ">
                         <div className="infoBubble">
                             <div className="bubbleTitle has-text-white">
                                 <p className='is-size-5'>Telefono</p>
@@ -63,7 +67,7 @@ export function DetallesCliente() {
                             </div>
                         </div>
                     </div>
-                    <div className="column clientInfoRight">
+                    <div className="clientInfoRight">
                         <div className="infoBubble">
                             <div className="bubbleTitle has-background-primary-dark has-text-white">
                                 <p className='is-size-5'>Plan</p>
@@ -97,18 +101,6 @@ export function DetallesCliente() {
                                 <td>{clienteMed.peso}</td>
                                 <td>{clienteMed.cintura}</td>
                                 <td>{clienteMed.piernas}</td>
-                            </tr>
-                            <tr>
-                                <td>-</td>
-                                <td>-</td>
-                                <td>-</td>
-                                <td>-</td>
-                            </tr>
-                            <tr>
-                                <td>-</td>
-                                <td>-</td>
-                                <td>-</td>
-                                <td>-</td>
                             </tr>
                         </tbody>
                     </table>
