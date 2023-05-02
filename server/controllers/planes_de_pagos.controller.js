72badeba6375ea0f6fad094645fe9761bcbeca21
Fernando Brizuela <ffba97@gmail.com>
2023-05-02 02:04:46 -0400

    Cambio de planes de pago a Objetos

--- server/controllers/planes_de_pagos.controller.js
+++ server/controllers/planes_de_pagos.controller.js
@@ -9,81 +9,107 @@ const {planes_de_pagos} = models;
 
 const clientes = new Clientes();
 
-/*
-Creamos un nuevo plan
-*/
-export const crearPlanDePago = async (req, res) => {
-    try {
-        // Verificamos si todos los campos estan
-        const validator = bodyValidator(req);
-        if (validator) return res.status(400).json(validator)
+export class PlanesDePagos {
+    /*
+    Creamos un nuevo plan
+    */
+    crear = async (req, res) => {
+        try {
+            // Verificamos si todos los campos estan
+            const validator = bodyValidator(req);
+            if (validator) return res.status(400).json(validator)
 
-        // destructuramos
-        const {body} = req;
+            // destructuramos
+            const {body} = req;
 
-        const {cliente_id} = body;
+            const {cliente_id} = body;
 
-        if (!(await clientes.getById(cliente_id))) return res.status(404).json({error: "No existe un usuario con ese ID"});
-        // Verificamos que el usuario ya no tenga un plan de pago
-        if (await getPlanPagoCliente(cliente_id)) return res.status(409).json({error: "El cliente ya posee un plan de pago"});
+            if (!(await clientes.getById(cliente_id))) return res.status(404).json({error: "No existe un usuario con ese ID"});
+            // Verificamos que el usuario ya no tenga un plan de pago
+            if (await getPlanPagoCliente(cliente_id)) return res.status(409).json({error: "El cliente ya posee un plan de pago"});
 
-        // Obtenemos el nombre del cliente
-        const {str_nombre} = await clientes.getById(cliente_id);
-        const str_modalidad = await getNombreModalidad(tipo_modalidad_de_pago_id);
-        const result = await planes_de_pagos.create({
-            ...body,
-            estado_de_pago: "pendiente",
-            date_fecha_de_vencimiento: toDate(date_fecha_de_vencimiento),
-            date_fecha_de_pago: null,
-            date_fecha_de_de_registro: getDateNow(),
-            date_fecha_de_actualizacion: getDateNow()
-        });
+            // Obtenemos el nombre del cliente
+            const {str_nombre} = await clientes.getById(cliente_id);
+            const str_modalidad = await getNombreModalidad(tipo_modalidad_de_pago_id);
+            const result = await planes_de_pagos.create({
+                ...body,
+                estado_de_pago: "pendiente",
+                date_fecha_de_vencimiento: toDate(date_fecha_de_vencimiento),
+                date_fecha_de_pago: null,
+                date_fecha_de_de_registro: getDateNow(),
+                date_fecha_de_actualizacion: getDateNow()
+            });
 
-        res.json(result)
-    } catch (error) {
-        res.status(500).json(error);
+            res.json(result)
+        } catch (error) {
+            res.status(500).json(error);
+        }
     }
-};
 
-// Devuelve todos los planes de pago registrados
-export const getAllPlanesDePagos = async (req, res) => {
-    try {
-        const result = await planes_de_pagos.findAll();
-        res.json(result);
-    } catch (error) {
-        res.json(error.message).status(500);
+    update = async (req, res) => {
+        try {
+            const {id} = body.params;
+            const {body} = body.req;
+            const [rowsAffected] = await planes_de_pagos.update({...body}, {where: {id}});
+            if (rowsAffected === 0) return res.status(404).json("No se actualizo ningun plan de pago");
+            res.status(200).send("Plan actualizado")
+        } catch (error) {
+            res.status(500).json(error);
+        }
     }
-};
 
-// Obtiene un plan de pago consultando por su ID
-export const getPlanDePagoByID = async (req, res) => {
-    try {
-        const {id} = req.params;
-        const result = await planes_de_pagos.findOne({where: {id}});
-        if (!result) return res.status(404).json('No existe un plan de pago con ese ID')
-        res.json(result)
-    } catch (error) {
-        res.json(error).status(500);
+    delete = async (req, res) => {
+        try {
+            const {id} = body.params;
+            if (!(await this.getById(id))) return res.status(404).json({error: "No existe ese plan de pago"});
+            await planes_de_pagos.destroy({where: {id}});
+            res.status(200).send("Plan de pago eliminado");
+        } catch (error) {
+            res.status(500).json(error);
+        }
     }
-}
 
-export const getPlanPagoClienteByParams = async (req, res) => {
-    try {
-        const {cliente_id} = req.params;
-        const result = await getPlanPagoCliente(cliente_id);
-        if (!result) return res.status(404).json({error: "No se encuentra un plan de pago con ese id de cliente"})
-        res.json(result)
-    } catch (error) {
-        res.status(500).json(error);
+    // Devuelve todos los planes de pago registrados
+    getAll = async (req, res) => {
+        try {
+            const result = await planes_de_pagos.findAll();
+            res.json(result);
+        } catch (error) {
+            res.status(500).json(error);
+        }
     }
-}
 
-// Devuelve un objeto si es que el usuario ya posee un plan de pago
-export const getPlanPagoCliente = async (cliente_id) => {
-    try {
-        const result = await planes_de_pagos.findOne({where: {cliente_id}});
-        return result;
-    } catch (error) {
-        throw new Error("Error al obtener plan de pago del cliente")
+    // Obtiene un plan de pago consultando por su ID
+    getByParams = async (req, res) => {
+        try {
+            const {id} = req.params;
+            const result = await planes_de_pagos.findOne({where: {id}});
+            if (!result) return res.status(404).json('No existe un plan de pago con ese ID')
+            res.json(result)
+        } catch (error) {
+            res.json(error).status(500);
+        }
     }
+
+    // Devuelve un objeto si es que el usuario ya posee un plan de pago
+    getPlanDePagoDeCliente = async (req, res) => {
+        try {
+            const {cliente_id} = req.params;
+            const result = await getPlanPagoCliente(cliente_id);
+            if (!result) return res.status(404).json({error: "No se encuentra un plan de pago con ese id de cliente"})
+            res.json(result)
+        } catch (error) {
+            res.status(500).json(error);
+        }
+    }
+
+    getById = async (req, res) => {
+        try {
+            const result = await planes_de_pagos.findOne({where: {id}});
+            return result;
+        } catch (error) {
+            return {error: "Algo salio mal"}
+        }
+    }
+
 }
--- server/routers/planes-de-pagos.routes.js
+++ server/routers/planes-de-pagos.routes.js
@@ -1,20 +1,23 @@
 import {Router} from "express"
 import {check} from "express-validator";
-import {
-    crearPlanDePago,
-    getAllPlanesDePagos,
-    getPlanPagoClienteByParams
-} from "../controllers/planes_de_pagos.controller.js";
+import {PlanesDePagos} from "../controllers/planes_de_pagos.controller.js";
+
+const planesDePagos = new PlanesDePagos();
 
 const planesPagosRoutes = Router();
 
-planesPagosRoutes.get('/planes-de-pagos', getAllPlanesDePagos);
+planesPagosRoutes.get('/planes-de-pagos', planesDePagos.getAll);
+
+planesPagosRoutes.get('/planes-de-pagos/:id', planesDePagos.getByParams);
 
 planesPagosRoutes.post('/planes-de-pagos', [
     check("cliente_id", "cliente_id  es un campo requerido").notEmpty(),
     check("tipo_modalidad_de_pago_id", "tipo_modalidad_de_pago_id  es un campo requerido").notEmpty(),
     check("date_fecha_de_vencimiento", "date_fecha_de_vencimiento  es un campo requerido").notEmpty(),
-    ], crearPlanDePago);
+    ], planesDePagos.crear);
 
+planesPagosRoutes.put('/planes-de-pagos/:id', planesDePagos.update)
+
+planesPagosRoutes.delete('/planes-de-pagos/:id', planesDePagos.delete)
 
 export default planesPagosRoutes;
