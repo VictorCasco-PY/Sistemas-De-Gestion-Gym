1d3fc617310ba0a0f4d787dde6045db02fc0cd1c
Fernando Brizuela <ffba97@gmail.com>
2023-05-04 17:26:55 -0400

    Agregado Modelo Controller

---
+++ server/controllers/MODELO CONTROLLER.js
@@ -0,0 +1,11 @@
+import { models } from "../models/models.js";
+
+
+export class Clase {
+    crear = async (req,res)=>{}
+    update = async (req,res)=>{}
+    delete = async(req,res)=>{}
+    getAll = async(req,res)=>{}
+    getByParams = async(req,res)=>{}
+    getById = async(req,res)=>{}
+}
