3767d47795d7aeee4e733aef1a7df1f16989d72a
Fernando Brizuela <ffba97@gmail.com>
2023-05-03 21:52:37 -0400

    Planes de pagos funcionando

--- server/controllers/planes_de_pagos.controller.js
+++ server/controllers/planes_de_pagos.controller.js
@@ -22,34 +22,33 @@ export class PlanesDePagos {
             const validator = bodyValidator(req);
             if (validator) return res.status(400).json(validator)
 
-            // destructuramos
             const {body} = req;
-
-            const {cliente_id} = body;
-
             const {tipo_modalidad_de_pago_id} = body;
-            const cliente = await clientes.getById(cliente_id);
-            const {str_nombre}= cliente;
-            if (!(cliente)) return res.status(404).json({error: "No existe un usuario con ese ID"});
-            // Verificamos que el usuario ya no tenga un plan de pago
-            if (!(await this.getPlanPagoCliente(cliente_id))) {return res.status(409).json({error: "El cliente ya posee un plan de pago"});}
-            // Obtenemos el nombre del cliente
-
+            const {cliente_id} = body;
             const str_modalidad = await tiposModalidadesDePagos.getNombreModalidad(tipo_modalidad_de_pago_id);
+            const str_nombre_cliente = (await clientes.getById(cliente_id)).str_nombre;
+
+
+            if (!(str_nombre_cliente)) return res.status(404).json({error: "No existe un usuario con ese ID"});
+            if (await this.getPlanPagoCliente(cliente_id)) {return res.status(409).json({error: "El cliente ya posee un plan de pago"});}
+            
+
+
             const result = await planes_de_pagos.create({
                 ...body,
                 str_modalidad,
-                str_nombre,
+                str_nombre_cliente,
                 estado_de_pago: "pendiente",
                 date_fecha_de_vencimiento: toDate(body.date_fecha_de_vencimiento),
                 date_fecha_de_pago: null,
                 date_fecha_de_de_registro: getDateNow(),
                 date_fecha_de_actualizacion: getDateNow()
             });
-            console.log(result);
+                  
+
+            
             res.json(result)
         } catch (error) {
-            console.log(error);
             res.status(500).json(error);
         }
     }
@@ -69,12 +68,13 @@ export class PlanesDePagos {
 
     delete = async (req, res) => {
         try {
-            const {id} = body.params;
+            const {id} = req.params;
             if (!(await this.getById(id))) return res.status(404).json({error: "No existe ese plan de pago"});
             await planes_de_pagos.destroy({where: {id}});
             res.status(200).send("Plan de pago eliminado");
         } catch (error) {
-            res.status(500).json(error);
+            console.log(error);
+            res.status(500).json({error});
         }
     }
 
@@ -104,7 +104,7 @@ export class PlanesDePagos {
     getPlanDePagoDeClienteByParams = async (req, res) => {
             try {
                 const {cliente_id} = req.params;
-                const result = await planes_de_pagos.findOne({where:{id}});
+                const result = await planes_de_pagos.findOne({where:{cliente_id}});
                 if (!result) return res.status(404).json({error: "No se encuentra un plan de pago con ese id de cliente"})
                 res.json(result)
             }catch (error){
@@ -116,7 +116,6 @@ export class PlanesDePagos {
     getPlanPagoCliente = async(cliente_id) => {
         try{
             const result = await planes_de_pagos.findOne({where:{cliente_id}});
-            if (!result) return res.status(404).json({error: "No se encuentra un plan de pago con ese id de cliente"})
             return result
         }catch(error){
             return {error: "Algo salio mal"};
@@ -125,13 +124,13 @@ export class PlanesDePagos {
 
 
 
-    getById = async (req, res) => {
+    getById = async (id) => {
         try {
+            
             const result = await planes_de_pagos.findOne({where: {id}});
-
-            res.json(result)
+            return result
         } catch (error) {
-            return {error: "Algo salio mal"}
+            res.status(500).json(error);
         }
     }
 
