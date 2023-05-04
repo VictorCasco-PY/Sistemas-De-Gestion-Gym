658fc646daddf1e438e23a703848c518c39f122a
Fernando Brizuela <ffba97@gmail.com>
2023-05-04 17:27:14 -0400

    Agregado Modelo Controller

--- server/controllers/MODELO CONTROLLER.js
+++ server/controllers/MODELO CONTROLLER.js
@@ -1,5 +1,7 @@
 import { models } from "../models/models.js";
+import {bodyValidator} from "../tools/bodyValidator.js";
 
+const {productos} = models;
 
 export class Clase {
     crear = async (req,res)=>{}
