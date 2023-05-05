c135b7edc25eee36e32eb483cd32725e28fd5b54
arvidjj <andy.oj14@gmail.com>
2023-05-05 02:10:59 -0400

    Arreglo menor

--- client/src/pages/ClientesTable.jsx
+++ client/src/pages/ClientesTable.jsx
@@ -3,18 +3,28 @@ import { Link } from 'react-router-dom';
 import axios from 'axios';
 
 export default function ClientesTable() {
-    const [clientes, setClientes] = useState([]);
+  const [clientes, setClientes] = useState([]);
+  const [filteredData, setFilteredData] = useState([]);
 
-    useEffect(() => {
-      axios.get("http://localhost:8000/planes-de-pagos").then((response) => {
-        setClientes(response.data);
-      });
-    }, []);
+  useEffect(() => {
+    axios.get("http://localhost:8000/planes-de-pagos").then((response) => {
+      setClientes(response.data);
+      setFilteredData(response.data);
+    });
+  }, []);
 
   const colorMap = {
     "En regla": "has-background-success",
     "pendiente": "has-background-warning has-text-black",
     "Atrasado": "has-background-danger"
+  };
+
+  
+  const handleFilter = (field) => {
+    console.log(filteredData)
+    const filtered = clientes.filter((item) => item[field]);
+    setFilteredData(filtered);
+    console.log(filteredData)
   };
 
   if (!clientes) {
@@ -25,14 +35,14 @@ export default function ClientesTable() {
     <table className="table table is-bordered tableNew has-background-light is-bordered">
       <thead className='has-text-centered'>
         <tr>
-          <th className='is-size-5'>Nombre</th>
-          <th className='is-size-6'>Plan</th>
-          <th className='is-size-6'>Estado de Pago</th>
+          <th className='is-size-5' onClick={() => handleFilter("str_nombre_cliente")}>Nombre</th>
+          <th className='is-size-6' onClick={() => handleFilter("str_modalidad")}>Plan</th>
+          <th className='is-size-6' onClick={() => handleFilter("estado_de_pago")}>Estado de Pago</th>
           <th className='is-size-6'></th>
         </tr>
       </thead>
       <tbody>
-        {clientes.map(cliente => (
+        {filteredData.map(cliente => (
           <tr key={cliente.cliente_id}>
             <td className='is-size-5'>{cliente.str_nombre_cliente}</td>
             <td className='is-size-5'>{cliente.str_modalidad}</td>
