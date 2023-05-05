f3a4b48eb8fc9a6b31aabea143bbfda1a17cc96f
Marcelo Ferreira <marceloferreira@fiuni.edu.py>
2023-05-05 14:57:11 -0400

    Aggregado el controller de Proveedores

--- server/controllers/productos.controller.js
+++ server/controllers/productos.controller.js
@@ -21,10 +21,53 @@ export class Productos {
       return res.json(error);
   }
 };
-  update = async (req, res) => {};
-  delete = async (req, res) => {};
-  getAll = async (req, res) => {};
-  getByParams = async (req, res) => {};
-  getById = async (req, res) => {};
+update = async (req, res) => {
+  try {
+      const {id} = req.params;
+      const {body} = req;
+      const [rowsAffected] = await productos.update({...body}, {where: {id}});
+      if (rowsAffected === 0) return res.status(404).json("No se actualizo ningun producto");
+      res.status(200).send("Producto Actualizado");
+  } catch (error) {
+      console.log(error)
+      return res.status(500).json({error})
+  }
+
+}
+delete = async (req, res) => {
+  try {
+      const {id} = req.params;
+      if (!(await this.getById(id))) return req.status(404).json({error: "No existe un producto con ese ID"});
+      await clientes.destroy({where: {id}});
+      res.status(200).send("Producto eliminado");
+  } catch (error) {
+      return res.status(500).json(error)
+  }
+}
+getAll = async (req, res) => {
+  try {
+      const result = await productos.findAll();
+      res.json(result);
+  } catch (error) {
+      res.json(error.message).status(500);
+  }
+}
+getByParams = async (req, res) => {
+  try {
+      const {id} = req.params;
+      const result = await this.getById(id);
+      return res.json(result);
+  } catch (error) {
+      res.json(error).status(500);
+  }
+}
+getById = async (id) => {
+  try {
+      const result = await productos.findOne({where: {id}});
+      return result;
+  } catch (error) {
+      throw new Error("Error al obtener cliente");
+  }
+}
   getByNombre= async (rq,res)=>{};
 }
