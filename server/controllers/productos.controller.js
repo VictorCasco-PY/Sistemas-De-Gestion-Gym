6017d48b2b99975b65ce537c3a7e4291a7049e41
Marcelo Ferreira <marceloferreira@fiuni.edu.py>
2023-05-05 10:36:22 -0400

    Commit productos controller

--- server/controllers/productos.controller.js
+++ server/controllers/productos.controller.js
@@ -3,18 +3,28 @@ import { bodyValidator } from "../tools/bodyValidator.js";
 
 const { productos } = models;
 
-export class Clase {
+export class Productos {
   crear = async (req, res) => {
-    const validator = bodyValidator(req);
-    if (validator) return res.status(400).json(validator);
+    try {
+      const validator = bodyValidator(req);
+      if (validator) return res.status(400).json(validator);
 
-    const {body} = req;
-    
+      const {body} = req;
+      const {str_nombre} = body;
 
-  };
+      if (await this.getByNombre({str_nombre})) return res.status(409).json({error: "El producto ya esta registrado"});
+
+      const result = await productos.create({...body});
+
+      res.json(result);
+  } catch (error) {
+      return res.json(error);
+  }
+};
   update = async (req, res) => {};
   delete = async (req, res) => {};
   getAll = async (req, res) => {};
   getByParams = async (req, res) => {};
   getById = async (req, res) => {};
+  getByNombre= async (rq,res)=>{};
 }
