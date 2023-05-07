2b114381840674d784df82f2a7e9090c0a84d591
Fernando Brizuela <ffba97@gmail.com>
2023-05-07 09:16:41 -0400

    Login creado

---
+++ server/tools/token.js
@@ -0,0 +1,36 @@
+import jwt from "jsonwebtoken";
+import {Empleado} from "../controllers/empleados.controller.js";
+import {bodyValidator} from "./bodyValidator.js";
+import bcrypt from "bcrypt";
+import {SECRET_KEY} from "../config.js";
+
+const empleado = new Empleado();
+
+export class Token {
+    crear = async (user, password) => {
+        try {
+            const userEmpleado = await empleado.getByUserName(user);
+
+            if (!userEmpleado) return res.status(404).json({error: "No existe ese usuario"});
+            const userPassword = userEmpleado.password;
+            const comparePassword = bcrypt.compareSync(password, userPassword);
+
+            if (!comparePassword) return null;
+
+            const payload = {id:userEmpleado.id, nombre:userEmpleado.str_nombre, rol:userEmpleado.rol}
+
+            const token = jwt.sign(payload, SECRET_KEY);
+
+            return {
+                token,
+                nombre:userEmpleado.str_nombre,
+                rol:userEmpleado.rol,
+                id:userEmpleado.id
+            }
+
+        } catch (error) {
+            console.log(error.message)
+            return null;
+        }
+    }
+}
