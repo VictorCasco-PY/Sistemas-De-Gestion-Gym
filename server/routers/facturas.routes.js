b32b715c79de67457c76366163a50ee804b33e41
Fernando Brizuela <ffba97@gmail.com>
2023-05-30 15:51:05 -0400

    trabajando en facturas

--- server/routers/facturas.routes.js
+++ server/routers/facturas.routes.js
@@ -7,7 +7,7 @@ const factura = new Factura();
 const facturasRoutes = Router();
 
 facturasRoutes.post('/facturas',
-    checkMiddleWare(['id_cliente','id_timbrado','numero_factura','date_fecha','str_nombre_cliente','str_ruc_cliente','condicion','total','saldo','iva_5','iva_10','iva_exenta']),
+    checkMiddleWare(['id_cliente','id_timbrado','numero_factura','date_fecha','str_nombre_cliente','str_ruc_cliente','total','saldo','iva_5','iva_10','iva_exenta']),
     factura.crear
 );
 
