d6e1e8dfd71a2953ac4adf146f691b2bc9b88968
VictorCasco-PY <cascoalmada@gmail.com>
2023-05-30 14:31:39 -0400

    Ventas actualizado

--- client/src/pages/Ventas.jsx
+++ client/src/pages/Ventas.jsx
@@ -15,14 +15,20 @@ const Ventas = () => {
 
     const getCliente = async (ruc) => {
         try {
-            const response = await axios.get(`http://localhost:8000/clientes?str_ruc=${ruc}`);
+            const user = JSON.parse(localStorage.getItem("user")); // Obtener el token del localStorage
+            const headers = {
+                token: user.token // Agregar el token en el encabezado 'token'
+            };
+
+            const response = await axios.get(`http://localhost:8000/clientes?str_ruc=${ruc}`, { headers });
             setCliente(response.data[0]);
             console.log(response.data[0]);
             id = response.data[0].id;
         } catch (error) {
             console.log(error.message);
         }
-    }
+    };
+
 
     const getPlanDePago = async (id) => {
         try {
@@ -77,15 +83,6 @@ const Ventas = () => {
         }
     };
 
-    const getProducto = async (id_producto) => {
-        try {
-            const response = await axios.get(`http://localhost:8000/producto/${id_producto}`);
-            setProductos([response.data]);
-            console.log(response.data);
-        } catch (error) {
-            alert("El producto no existe");
-        }
-    }
 
     const handleBuscadorChange = (event) => {
         setIdProducto(event.target.value);
@@ -95,6 +92,7 @@ const Ventas = () => {
         if (event.key === 'Enter') {
             try {
                 handleAddProducto();
+                setIdProducto('')
             } catch (error) {
                 console.log(error);
                 return;
@@ -111,99 +109,94 @@ const Ventas = () => {
 
     const navigate = useNavigate();
     return (
-        <div className='container box columns m-4'>
-            <button className='button is-info' onClick={() => navigate(-1)}><ArrowBackIcon fontSize='string' /></button>
-            <div className='column'>
-                <div className='column is-one-third'>
-                    <h3>Datos del cliente</h3>
-                    <label htmlFor="str_ruc">Nro Documento</label>
-                    <input className='input is-primary' type="text" name="str_ruc" onChange={handleRucChange} onKeyDown={handleKeyDown} />
-                    <label htmlFor="str_nombre">Nombre</label>
-                    <input className='input is-primary' type="text" name="str_nombre" readOnly value={cliente?.str_nombre || ' '} />
-                </div>
-                <div className='mt-5'>
-                    <h3>Productos/Servicios</h3>
-                    <input className='input is-primary' type="text" name="buscador" placeholder='Buscar producto/servicio' value={idProducto} onChange={handleBuscadorChange} onKeyDown={handleKeyDownBuscador} />
-                    <button className='button is-primary mt-2 mr-2' onClick={handleAddProducto}>Agregar</button>
-                    <button className='button is-danger mt-2' onClick={() => setIdProducto('')}>Limpiar</button>
-                    <table>
-                        <thead>
-                            <tr>
-                                <td></td>
-                                <th>Producto/Servicio</th>
-                                <th>Cantidad</th>
-                                <th>Precio(Gs.)</th>
-                                <th>Total(Gs.)</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {planDePago.estado_de_pago === 'pendiente' && (
+        <>
+            <h1 className='title is-1'>Nueva Venta</h1>
+            <div className='container box columns m-4'>
+                <button className='button is-info' onClick={() => navigate(-1)}><ArrowBackIcon fontSize='string' /></button>
+                <div className='column'>
+                    <div className='column is-one-third'>
+                        <h3>Datos del cliente</h3>
+                        <label htmlFor="str_ruc">Nro Documento</label>
+                        <input className='input is-primary' type="text" name="str_ruc" onChange={handleRucChange} onKeyDown={handleKeyDown} />
+                        <label htmlFor="str_nombre">Nombre</label>
+                        <input className='input is-primary' type="text" name="str_nombre" readOnly value={cliente?.str_nombre || ' '} />
+                    </div>
+                    <div className='mt-5'>
+                        <h3>Productos/Servicios</h3>
+                        <input className='input is-primary' type="text" name="buscador" placeholder='Buscar producto/servicio' value={idProducto} onChange={handleBuscadorChange} onKeyDown={handleKeyDownBuscador} />
+                        <button className='button is-info mt-2 mr-2' onClick={handleAddProducto}>Agregar</button>
+                        <table>
+                            <thead>
                                 <tr>
-                                    <td><button className='button is-danger' onClick={handleDeletePlanDePago}><DeleteIcon fontSize='string' /></button></td>
-                                    <td>Pago Cuota {planDePago.str_modalidad}</td>
-                                    <td>1</td>
-                                    <td>{planDePago.id_tipo_modalidad_de_pago === 1 ? '10.000' : planDePago.id_tipo_modalidad_de_pago === 2 ? '70.000' : planDePago.id_tipo_modalidad_de_pago === 3 ? '100.000' : ''}</td>
-                                    <td>{planDePago.id_tipo_modalidad_de_pago === 1 ? '10.000' : planDePago.id_tipo_modalidad_de_pago === 2 ? '70.000' : planDePago.id_tipo_modalidad_de_pago === 3 ? '100.000' : ''}</td>
+                                    <td></td>
+                                    <th>Producto/Servicio</th>
+                                    <th>Cantidad</th>
+                                    <th>Precio(Gs.)</th>
+                                    <th>Total(Gs.)</th>
                                 </tr>
-                            )}
-                            {productos.map((producto, index) => (
-                                <tr key={index}>
-                                    <td>
-                                        <button className='button is-danger' onClick={() => handleDeleteProducto(index)}>
-                                            <DeleteIcon fontSize='string' />
-                                        </button>
-                                    </td>
-                                    <td>{producto.str_descripcion}</td>
-                                    <td>
-                                        <input
-                                            type="number"
-                                            min="1"
-                                            value={producto.cantidad}
-                                            onChange={(event) => handleCantidadChange(event, index)}
-                                        />
-                                    </td>
-                                    <td>{producto.precio}</td>
-                                    <td>{producto.cantidad * producto.precio}</td>
-                                </tr>
-                            ))}
-                        </tbody>
-                    </table>
-                </div>
-            </div>
-            <div className='column is-one-third'>
-                <div>
-                    <h3>Detalle de venta</h3>
-                    <div className='select is-primary'>
-                        <select name="tipo" id="tipo">
-                            <option>Tipo</option>
-                            <option value="factura">Credito</option>
-                            <option value="ticket">Efectivo</option>
-                        </select>
-                    </div>
-                    <div>
-                        <label htmlFor="tipo">Sub Total</label>
-                        <input type="text" className='input is-primary' disabled value={45000} />
-                    </div>
-                    <div>
-                        <label htmlFor="tipo">IVA (5%)</label>
-                        <input type="text" className='input is-primary' disabled />
-                    </div>
-                    <div>
-                        <label htmlFor="tipo">IVA (10%)</label>
-                        <input type="text" className='input is-primary' disabled value={5000} />
-                    </div>
-                    <div>
-                        <label htmlFor="tipo">Total</label>
-                        <input type="text" className='input is-primary' disabled value={50000} />
-                    </div>
-                    <div className='buttons is-right'>
-                        <button className='button is-primary mt-6'>
-                            <PaymentIcon fontSize='medium' />Finalizar Venta
-                        </button>
+                            </thead>
+                            <tbody>
+                                {planDePago.estado_de_pago === 'pendiente' && (
+                                    <tr>
+                                        <td><button className='button' onClick={handleDeletePlanDePago}><DeleteIcon fontSize='string' /></button></td>
+                                        <td>Pago Cuota {planDePago.str_modalidad}</td>
+                                        <td>1</td>
+                                        <td>{planDePago.id_tipo_modalidad_de_pago === 1 ? '10.000' : planDePago.id_tipo_modalidad_de_pago === 2 ? '70.000' : planDePago.id_tipo_modalidad_de_pago === 3 ? '100.000' : ''}</td>
+                                        <td>{planDePago.id_tipo_modalidad_de_pago === 1 ? '10.000' : planDePago.id_tipo_modalidad_de_pago === 2 ? '70.000' : planDePago.id_tipo_modalidad_de_pago === 3 ? '100.000' : ''}</td>
+                                    </tr>
+                                )}
+                                {productos.map((producto, index) => (
+                                    <tr key={index}>
+                                        <td>
+                                            <button className='button' onClick={() => handleDeleteProducto(index)}>
+                                                <DeleteIcon fontSize='string' />
+                                            </button>
+                                        </td>
+                                        <td>{producto.str_descripcion}</td>
+                                        <td>
+                                            <input
+                                                type="number"
+                                                min="1"
+                                                value={producto.cantidad}
+                                                onChange={(event) => handleCantidadChange(event, index)}
+                                            />
+                                        </td>
+                                        <td>{producto.precio}</td>
+                                        <td>{producto.cantidad * producto.precio}</td>
+                                    </tr>
+                                ))}
+                            </tbody>
+                        </table>
                     </div>
                 </div>
+                <div className='column is-one-third'>
+                    <div>
+                        <h3>Detalle de venta</h3>
+                        <div>
+                            <label htmlFor="tipo">Sub Total</label>
+                            <input type="text" className='input is-primary' disabled value={45000} />
+                        </div>
+                        <div>
+                            <label htmlFor="tipo">IVA (5%)</label>
+                            <input type="text" className='input is-primary' disabled />
+                        </div>
+                        <div>
+                            <label htmlFor="tipo">IVA (10%)</label>
+                            <input type="text" className='input is-primary' disabled value={5000} />
+                        </div>
+                        <div>
+                            <label htmlFor="tipo">Total</label>
+                            <input type="text" className='input is-primary' disabled value={50000} />
+                        </div>
+                        <div className='buttons is-right'>
+                            <button className='button is-primary mt-6'>
+                                <PaymentIcon fontSize='medium' />Guardar Venta
+                            </button>
+                        </div>
+                    </div>
+                </div>
             </div>
-        </div>
+        </>
     )
 }
 
