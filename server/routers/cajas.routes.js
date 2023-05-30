49e0b04c768e8cd7fc9e1bc226849e0c34cd3b2b
Marcelo Ferreira <marceloferreira@fiuni.edu.py>
2023-05-30 14:10:10 -0400

    Commit arreglado

--- server/routers/cajas.routes.js
+++ server/routers/cajas.routes.js
@@ -2,6 +2,8 @@ import { Router } from "express";
 import { check } from "express-validator";
 
 import { Cajas } from "../controllers/cajas.controller.js";
+import { authMiddleware } from "../middlewares/authMiddleware.js";
+import { checkMiddleWare } from "../middlewares/checkMiddleware.js";
 
 
 
@@ -16,7 +18,13 @@ cajasRoutes.get("/cajas", cajas.getAll);
 cajasRoutes.get("/caja/:id", cajas.getByParams);
 
 // Crear un nuevo producto, los checks sirven para comprobar que los campos esten completos
-cajasRoutes.post('/cajas',checkMiddleWare(['str_nombre']), cajas.crear);
+cajasRoutes.post(
+    "/cajas",
+    authMiddleware(['caja'])
+    ,checkMiddleWare(['str_nombre']),
+    cajas.crear
+);
+
 
 cajasRoutes.put("/caja/:id", cajas.update)
 cajasRoutes.delete("/caja/:id", cajas.delete)
