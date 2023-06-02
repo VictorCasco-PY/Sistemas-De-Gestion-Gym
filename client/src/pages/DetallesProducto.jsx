23be74859570463ed5282e19e69a8167985a4c5d
JulioLaneri <juliobenitez@fiuni.edu.py>
2023-06-02 13:33:39 -0400

    Filtrados

---
+++ client/src/pages/DetallesProducto.jsx
@@ -0,0 +1,105 @@
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import EditableInput from "../components/EditableInput";
+import api from "../services/api";
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
+import { Link } from "react-router-dom";
+
+const DetallesProducto = () => {
+  const id = useParams().id;
+  const [productos, setproductos] = useState(null);
+
+  // Función para formatear la cadena de tiempo (hh:mm)
+
+  useEffect(() => {
+    api
+      .get(`producto/${id}`)
+      .then((response) => {
+        setproductos(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  if (!productos) {
+    return <div>Cargando detalles del producto...</div>;
+  }
+
+  return (
+    <div className="box">
+      <div style={{ display: "flex", alignItems: "center" }}>
+        <Link to={`/listaProductos`}>
+          <button className="button is-link is-outlined mr-6">
+            <ArrowBackIcon fontSize="string" />
+          </button>
+        </Link>
+        <h1 className="title is-3 has-text-primary">Detalles del Producto</h1>
+      </div>
+      <div className="columns">
+        <div className="column">
+          <h4>Nombre</h4>
+          <EditableInput
+            valorInicial={productos.str_nombre}
+            id={id}
+            apiUrl="http://localhost:8000/producto"
+            campoCambiar="str_nombre"
+          />
+        </div>
+        <div className="column">
+          <h4>Precio</h4>
+          <EditableInput
+            valorInicial={productos.precio}
+            id={id}
+            apiUrl="http://localhost:8000/producto"
+            campoCambiar="precio"
+          />
+        </div>
+      </div>
+      <div className="columns">
+        <div className="column">
+          <h4>IVA</h4>
+          <EditableInput
+            valorInicial={productos.iva}
+            id={id}
+            apiUrl="http://localhost:8000/producto"
+            campoCambiar="iva"
+          />
+        </div>
+        <div className="column">
+          <h4>Descripción</h4>
+          <EditableInput
+            valorInicial={productos.str_descripcion}
+            id={id}
+            apiUrl="http://localhost:8000/producto"
+            campoCambiar="str_descripcion"
+          />
+        </div>
+      </div>
+      <div className="columns">
+        <div className="column">
+          <h4>Codigo</h4>
+          <EditableInput
+            valorInicial={productos.str_codigo}
+            id={id}
+            apiUrl="http://localhost:8000/producto"
+            campoCambiar="str_codigo"
+          />
+        </div>
+        <div className="column">
+          <h4>Cantidad</h4>
+          <div className="column">
+            <EditableInput
+              valorInicial={productos.cantidad}
+              id={id}
+              apiUrl="http://localhost:8000/producto"
+              campoCambiar="cantidad"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default DetallesProducto;
--- client/src/pages/RegistroProductos.jsx
+++ client/src/pages/RegistroProductos.jsx
@@ -1,30 +1,50 @@
 import React, { useState } from "react";
 import Swal from "sweetalert2";
 import api from "../services/api";
+import { Link } from "react-router-dom";
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 
 const RegistroProductos = () => {
   const productosData = {
     str_descripcion: "",
     precio: "",
-    iva: "",
+    iva: null, // Valor inicial null
     str_nombre: "",
     str_codigo: "",
     cantidad: "",
   };
+
   const [productos, setProductos] = useState(productosData);
 
+  const opcionesIva = [
+    { label: "IVA", value: null }, // Opción de placeholder
+    { label: "0%", value: "0" },
+    { label: "5%", value: "5" },
+    { label: "10%", value: "10" },
+  ];
+
   const handleChange = (event) => {
+    const { name, value } = event.target;
     setProductos({
       ...productos,
-      [event.target.name]: event.target.value,
+      [name]: value,
     });
   };
 
   // función para manejar el envío del formulario
   const handleSubmit = (event) => {
     event.preventDefault();
-    api.post('/productos', productos)
-      .then(response => {
+    if (productos.iva === null || productos.cantidad < 0) {
+      Swal.fire({
+        icon: "error",
+        title: "Error al registrar el producto",
+        text: "Por favor, verifica los datos e intenta nuevamente",
+      });
+      return;
+    }
+    api
+      .post("/productos", productos)
+      .then((response) => {
         console.log(response.data);
         Swal.fire({
           icon: "success",
@@ -35,23 +55,34 @@ const RegistroProductos = () => {
         });
         setProductos(productosData); //restablecer a su valor inicial
       })
-      .catch(error => {
+      .catch((error) => {
         console.log(error);
         Swal.fire({
           icon: "error",
-          title: "Error al registrar el usuario",
+          title: "Error al registrar el producto",
           text: "Por favor, verifica los datos e intenta nuevamente",
         });
       });
   };
 
   return (
-    <div className="columns is-centered">
+<div className="columns is-centered">
       <div className="column is-three-fifths-desktop">
         <div className="box">
           <form onSubmit={handleSubmit}>
+          <Link to="/listaProductos">
+                <button
+                  className="button is-link is-outlined"
+                  style={{ display: "inline-block" }}
+                >
+                  <ArrowBackIcon fontSize="string" />
+                </button>
+              </Link>
             <center>
-              <h1 className="mb-2 title is-3 has-text-primary">
+              <h1
+                className="mb-2 title is-3 has-text-primary"
+                style={{ display: "inline-block", marginRight: "10px" }}
+              >
                 Registrar Producto
               </h1>
             </center>
@@ -95,11 +126,11 @@ const RegistroProductos = () => {
               <div className="column">
                 <input
                   className="input is-primary has-text-centered"
-                  type="text"
-                  name="iva"
-                  value={productos.iva}
+                  type="number"
+                  name="cantidad"
+                  value={productos.cantidad}
                   onChange={handleChange}
-                  placeholder="IVA"
+                  placeholder="Cantidad"
                   required
                 />
               </div>
@@ -116,19 +147,23 @@ const RegistroProductos = () => {
                   required
                 />
               </div>
-              <div className="column">
-                <input
-                  className="input is-primary has-text-centered"
-                  type="number"
-                  name="cantidad"
-                  value={productos.cantidad}
+              <div className="column select is-primary">
+                <select
+                  className="has-text-centered"
+                  name="iva"
+                  value={productos.iva || ""}
                   onChange={handleChange}
-                  placeholder="Cantidad"
                   required
-                />
+                >
+                  {opcionesIva.map((opcion) => (
+                    <option key={opcion.value} value={opcion.value}>
+                      {opcion.label}
+                    </option>
+                  ))}
+                </select>
               </div>
             </div>
-              <div className="buttons">
+            <div className="buttons">
               <button
                 className="ml-auto mt-2 button is-primary is-outlined"
                 type="submit"
