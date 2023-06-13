8e40cd5a014a401cf379d6752ce733a2f62086fc
Fernando Brizuela <ffba97@gmail.com>
2023-06-13 19:47:24 -0400

    Avances en logica de cobros

--- server/services/venta.js
+++ server/services/venta.js
@@ -1,8 +1,10 @@
 import { Factura } from "../controllers/facturas.controller.js";
 import { FacturaDetalle } from "../controllers/facturas_detalles.controller.js";
+import {CobrosDetalles} from "../controllers/cobros_detalles.controller.js";
 import {Cobros} from "../controllers/cobros.controller.js"
 import {getDateNow, getHourNow} from "../tools/date.js";
 
+const cobroDetalleController = new CobrosDetalles();
 const cobroController = new Cobros();
 const facturaController = new Factura();
 const facturaDetalleController = new FacturaDetalle();
@@ -12,23 +14,30 @@ export class Venta {
     let factura, factura_detalle;
     try {
 
-        //     checkMiddleWare(['id_cliente','id_timbrado','total','saldo','iva_5','iva_10','iva_exenta']),
-        const {id_cliente, id_timbrado, total, saldo, iva_5, iva_10, iva_exenta, detalles} = req.body;
+        const {id_cliente, id_timbrado, total, saldo, iva_5,iva_10, iva_exenta, detalles, cobros_detalles} = req.body;
 
         factura = await facturaController.nuevaFactura({id_cliente, id_timbrado, total, saldo, iva_5, iva_10, iva_exenta});
 
         detalles.id_factura = factura.id;	//guardar el id de la factura
-        factura_detalle = await facturaDetalleController.nuevaFacturaDetalle(detalles);
+        await facturaDetalleController.nuevaFacturaDetalle(detalles); // se guardan los detalles
 
-        let cobro = {};
-        cobro.id_factura = factura.id;
-        cobro.id_sesion_caja = null;
-        cobro.date_fecha = getDateNow();
-        cobro.time_hora = getHourNow();
+        const cobro = {
+            id_factura: factura.id,
+            id_sesion_caja: nul,
+            date_fecha: getDateNow(),
+            time_hora: getHourNow(),
+        };
 
-        await cobroController.nuevoCobro(cobro);
+        const nuevoCobro = await cobroController.nuevoCobro(cobro);
+
+        cobros_detalles.id_cobro = nuevoCobro.id;
+        await cobroDetalleController(...cobros_detalles);
+
+        console.log('Nuevo cobro: ');
+        console.log()
 
         const nuevaFactura = facturaController.getById(factura.id);
+
 
         res.status(200).json({nuevaFactura});
 
