4a31275b18e2d579e7df4e334649848711be71ba
arvidjj <andy.oj14@gmail.com>
2023-06-14 19:03:38 -0400

    ajustado tamanho de paginas

--- client/src/pages/facturas/ListaFacturas.jsx
+++ client/src/pages/facturas/ListaFacturas.jsx
@@ -89,8 +89,9 @@ const ListaFacturas = () => {
     };
 
     return (
-        <div className='is-flex is-flex-direction-column p-5 pageMain has-background-light'>
-            <div className="columns column is-four-fifths is-flex is-flex-direction-column">
+        <div className='is-flex is-flex-direction-column p-5 has-background-light ml-auto mr-auto'
+        style={{border: "1px solid #D4D4D4", borderRadius: "8px", maxWidth:"1500px"}}>
+            <div className="columns column is-flex is-flex-direction-column">
                 <div className="column is-flex is-flex-direction-column">
                     {activeId !== null && (
                         <DetalleFactura id={activeId} onClose={closeModal} />
--- client/src/pages/productos/ListaProducto.jsx
+++ client/src/pages/productos/ListaProducto.jsx
@@ -9,9 +9,11 @@ const ListaProducto = () => {
         <div className='column'>
             <div className="headerTitle">
                 <h1 className='title is-size-2'>Lista de Productos</h1>
+                <hr />
             </div>
-            <div className='is-flex is-flex-direction-column p-5 pageMain has-background-light'>
-                <div className="columns column is-four-fifths is-flex is-flex-direction-column">
+            <div className='is-flex is-flex-direction-column p-5 has-background-light ml-auto mr-auto'
+            style={{border: "1px solid #D4D4D4", borderRadius: "8px", maxWidth:"1500px"}}>
+                <div className="columns column is-flex is-flex-direction-column">
                     <div className='column is-flex is-justify-content-flex-end m-0 p-0'>
                         <Link className="custom-link is-flex" to="/registroProducto"><button className='button is-success'><AddIcon /> Agregar Producto</button></Link>
                     </div>
