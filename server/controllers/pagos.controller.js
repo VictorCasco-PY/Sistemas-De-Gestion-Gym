acb10ccced23c9e2f1e446560a29fbff2d6e8534
Marcelo Ferreira <marceloferreira@fiuni.edu.py>
2023-06-14 08:35:31 -0400

    Pagos y detalles

--- server/controllers/pagos.controller.js
+++ server/controllers/pagos.controller.js
@@ -1,15 +1,15 @@
 import { models } from "../models/models.js";
 import { getDateNow } from "../tools/date.js"
-import { Proveedores } from "./proveedores.controller.js";
-const { facturas_proveedores } = models;
-const proveedor = new Proveedores();
+import { Facturas_proveedores } from "./facturas_proveedores.controller.js";
+const { pagos_proveedores } = models;
+const factura_proveedor = new Facturas_proveedores();
 
 
-export class Facturas_proveedores {
+export class Pagos_proveedores {
     crear = async (req, res) => {
         try {
             const { body } = req;
-            const result = await this.createFacturaProveedor(...body);
+            const result = await this.createPagoProveedor(...body);
             return res.json({result});
         } catch (error) {
             const { message } = error;
@@ -19,14 +19,10 @@ export class Facturas_proveedores {
 
 
 
-    createFacturaProveedor = async (query) => {
+    createPagoProveedor = async (query) => {
         try {
-            const{id_proveedor}=query;
-            const proveedorTemp=await proveedor.getById(id_proveedor);
-            if(!proveedorTemp) return res.status(404).json({erorr:"No existe proveedor con ese id"}); 
-            const {str_nombre}=proveedorTemp.str_nombre;
-            const {str_ruc}= proveedorTemp.str_ruc;
-            const result = await facturas_proveedores.create({ ...query, str_nombre, str_ruc,nro_factura,date_fecha });
+            const{id_factura_proveedor}=query;
+            const result = await pagos_proveedores.create({ ...query, id_factura_proveedor });
             return result;
         } catch (error) {
             throw new Error("Error al crear la factura-proveedor");
@@ -36,11 +32,11 @@ export class Facturas_proveedores {
         try {
             const { id } = req.params;
             const { body } = req;
-            const [rowsAffected] = await facturas_proveedores.update({ ...body }, { where: { id } });
+            const [rowsAffected] = await pagos_proveedores.update({ ...body }, { where: { id } });
             if (rowsAffected === 0) {
-                return res.status(404).json("No se actualizó ninguna factura-proveedor");
+                return res.status(404).json("No se actualizó ningun pago-proveedor");
             }
-            res.status(200).send("factura-proveedor actualizada");
+            res.status(200).send("pago-proveedor actualizado");
         } catch (error) {
             console.log(error);
             return res.status(500).json({ error });
@@ -49,7 +45,7 @@ export class Facturas_proveedores {
     delete = async (req, res) => {
         try {
             const { id } = req.params;
-            const rowsAffected = await facturas_proveedores.destroy({ where: { id } });
+            const rowsAffected = await pagos_proveedores.destroy({ where: { id } });
             if (rowsAffected === 0) {
                 return res.status(404).json("No existe una factura-proveedor con ese ID");
             }
@@ -74,7 +70,7 @@ export class Facturas_proveedores {
             // Add date range filtering
             if (startDate && endDate) { where.date_fecha = { [Op.between]: [new Date(startDate), new Date(endDate)] }; }
 
-            const result = await facturas_proveedores.findAll({ where, ...options }) || facturas_proveedores.findAll({ where });
+            const result = await pagos_proveedores.findAll({ where, ...options }) || pagos_proveedores.findAll({ where });
 
             res.json(result);
         } catch (error) {
@@ -85,10 +81,10 @@ export class Facturas_proveedores {
         try {
             const { id } = req.params;
             console.log("Se busca el  id");
-            const result = await facturas_proveedores.findOne({ where: { id } });
+            const result = await pagos_proveedores.findOne({ where: { id } });
 
             if (!result) {
-                return res.status(404).json("No se encontró una factura-proveedor con ese ID");
+                return res.status(404).json("No se encontró una pago-proveedor con ese ID");
             }
 
             res.json(result);
--- server/services/compra.js
+++ server/services/compra.js
@@ -1,18 +1,32 @@
 import { Facturas_proveedores } from "../controllers/facturas_proveedores.controller.js";
 import { Facturas_proveedores_detalles } from "../controllers/facturas_proveedores_detalles.controller.js";
+import { Pagos_proveedores } from "../controllers/pagos.controller.js";
+import { Pagos_proveedores_detalles } from "../controllers/pagos_detalles.controller.js";
 
 const facturaProveedorController = new Facturas_proveedores();
 const facturaProveedorDetalleController = new Facturas_proveedores_detalles();
+const pagoController = new Pagos_proveedores();
+const pagoDetallesController = new Pagos_proveedores_detalles();
 
 export class Compra {
     crear = async (req, res) => {
         let factura_proveedor, factura_proveedor_detalle;
         try {
-            const { id_proveedor, str_nombre, _str_ruc, total, detalles } = req.body;
-            factura_proveedor = await facturaProveedorController.createFacturaProveedor({ id_proveedor, str_nombre, _str_ruc, total });
+            const { id_proveedor, date_fecha, nro_factura, str_nombre, _str_ruc, total, detalles, pagos_detalles } = req.body;
+            factura_proveedor = await facturaProveedorController.createFacturaProveedor({ id_proveedor, date_fecha, nro_factura, str_nombre, _str_ruc, total });
+            console.log("Hecho Factura");
             detalles.id_factura_proveedor = factura_proveedor.id;
             factura_proveedor_detalle = await facturaProveedorDetalleController.createFacturaProveedorDetalle(detalles);
-            res.status(200).json({ ok: "Compra realizada correctamente" })
+            console.log("hecho factura_detalles");
+
+            const pago = { id_factura_proveedor: factura_proveedor.id, id_sesion_caja: null, date_fecha }
+            const nuevoPago = await pagoController.createPagoProveedor(pago);
+            console.log("Hecho pago");
+            await  pagoDetallesController.createPagoProveedorDetalle(...pagos_detalles);
+            console.log("nuevo pago");
+            console.log(nuevoPago);
+            nuevaFactura=facturaProveedorController.getById(factura_proveedor.id);
+            res.status(200).json({ nuevaFactura })
 
         } catch (error) {
             res.status(500).json({ error: error.message });
