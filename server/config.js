3e58d90e8e130a8fb78e0e7051a1fc98253ef68b
Fernando Brizuela <ffba97@gmail.com>
2023-06-16 11:57:31 -0400

    Se mudo a una DB nuevita y lindita. VICTOR NO LLORES

--- server/config.js
+++ server/config.js
@@ -7,9 +7,9 @@
 
 export const DB_NAME = process.env.DB_NAME || 'railway';
 export const DB_USER = process.env.DB_USER || 'root';
-export const DB_PASS = process.env.DB_PASS || '4VYwHZ6fHE9yFSUFVsEL'
-export const DB_HOST = process.env.DB_HOST || 'containers-us-west-73.railway.app';
-export const DB_PORT = process.env.DB_PORT || 7328
+export const DB_PASS = process.env.DB_PASS || 'wfAR45IXnLmJ0AJpSZpp'
+export const DB_HOST = process.env.DB_HOST || 'containers-us-west-78.railway.app';
+export const DB_PORT = process.env.DB_PORT || 7257
 
 
 export const SECRET_KEY = 'LrL!;37O=Zk[#WB%ROizNBVVwyi]mh';
