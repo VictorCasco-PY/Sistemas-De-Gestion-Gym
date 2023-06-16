0d4eba627da0b15c74cc8045a230cd38918f471c
Marcelo Ferreira <marceloferreira@fiuni.edu.py>
2023-06-16 09:45:30 -0400

    Intento fallido arqueo

--- server/routers/arqueoCaja.routes.js
+++ server/routers/arqueoCaja.routes.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { getArqueo } from '../services/arqueoCaja';
+import { Router } from 'express';
+import { getArqueo } from '../services/arqueoCaja.js';
 
 const arqueoCajaRoutes = Router();
 
 // Ruta para obtener el arqueo
-arqueoCajaRoutes.get('/arqueo', getArqueo);
+arqueoCajaRoutes.get('/arqueoCaja/:id', getArqueo);
 
 export default arqueoCajaRoutes;
--- server/services/arqueoCaja.js
+++ server/services/arqueoCaja.js
@@ -1,36 +1,43 @@
 import { models } from "../models/models.js";
-
+import Sequelize from 'sequelize';
 const { pagos_proveedores, sesiones_cajas, cobros } = models;
 
 
 export const getArqueo = async (req, res) => {
     try {
-        const { id } = req.body;
-        const result = await sesiones_cajas.findAll({
-            include: [
-                { 
-                    model: pagos_proveedores,
-                    attributes: [],
-                    required: true
-                },
-                { 
-                    model: cobros,
-                    attributes: [],
-                    required: true
-                }
-            ],
-            where: { id },
+      const { id } = req.body;
+      const result = await sesiones_cajas.findOne({
+        include: [
+          {
+            model: pagos_proveedores,
             attributes: [
-                [sequelize.literal('(SELECT SUM(total) FROM pagos_proveedores WHERE sesiones_cajas.id = pagos_proveedores.id_sesion_caja)'), 'totalPagos'],
-                [sequelize.literal('(SELECT SUM(total) FROM cobros WHERE sesiones_cajas.id = cobros.id_sesion_caja)'), 'totalCobros']
-            ]
-        });
-
-        const { totalPagos, totalCobros } = result[0].toJSON();
-        const arqueo = totalCobros - totalPagos;
-
-        res.json({ totalPagos, totalCobros, arqueo });
+              [Sequelize.literal('COALESCE(SUM(pagos_proveedores.total), 0)'), 'totalPagos']
+            ],
+            required: true
+          },
+          {
+            model: cobros,
+            attributes: [
+              [Sequelize.literal('COALESCE(SUM(cobros.total), 0)'), 'totalCobros']
+            ],
+            required: true
+          }
+        ],
+        where: { id },
+        attributes: []
+      });
+  
+      if (!result) {
+        // Handle the case where no results are found
+        return res.status(404).json({ message: 'No arqueo found for the given ID' });
+      }
+  
+      const { totalPagos, totalCobros } = result.toJSON();
+      const arqueo = totalCobros - totalPagos;
+  
+      res.json({ totalPagos, totalCobros, arqueo });
     } catch (error) {
-        res.status(500).send(error.message);
+      res.status(500).send(error.message);
     }
-}
+  }
+  
