8d4f5d8a9ab7236b48c2ca6d9bc7d0dda0f74fc0
JulioLaneri <juliobenitez@fiuni.edu.py>
2023-06-16 11:03:44 -0400

    validacion en sesion caja

--- client/src/pages/SesionCaja.jsx
+++ client/src/pages/SesionCaja.jsx
@@ -1,24 +1,28 @@
 import React, { useState, useEffect } from 'react';
 import { format } from 'date-fns';
+import Swal from 'sweetalert2';
 import api from '../services/api';
 
 const SesionCaja = () => {
   const [abrirCaja, setAbrirCaja] = useState(false);
   const [montoInicial, setMontoInicial] = useState('');
+  const [montoFinal, setMontoFinal] = useState('');
   const [horaCierre, setHoraCierre] = useState('');
 
-  // Al cargar el componente, verifica si existe una sesión de caja en el almacenamiento local
   useEffect(() => {
     const sesionCajaId = localStorage.getItem('sesionCajaId');
     if (sesionCajaId) {
-      // Si hay una sesión de caja existente, significa que la caja está abierta
       setAbrirCaja(true);
     }
   }, []);
 
   const handleAbrirCerrarCaja = () => {
     if (!abrirCaja) {
-      // Si la caja está cerrada, se hace clic en "Abrir Caja"
+      if (montoInicial === '' || Number(montoInicial) < 0) {
+        Swal.fire('Error', 'El monto inicial no puede ser menor a 0', 'error');
+        return;
+      }
+
       const fechaApertura = new Date();
       setAbrirCaja(true);
 
@@ -33,36 +37,46 @@ const SesionCaja = () => {
         time_cierre: format(fechaApertura, 'HH:mm'),
       };
 
-      // Realizar la solicitud POST para abrir la caja
       api.post('/sesiones-cajas', data)
         .then(response => {
           console.log(response.data);
           localStorage.setItem('sesionCajaId', response.data.id);
         })
-        .catch(error => { console.log(error) });
+        .catch(error => {
+          console.log(error);
+        });
 
-      } else {
-        // Si la caja está abierta, se hace clic en "Cerrar Caja"
-        setMontoInicial('');
-        const fechaCierre = new Date();
-        setHoraCierre(fechaCierre.toLocaleTimeString());
-      
-        // Obtener el ID de la sesión de caja
-        const sesionCajaId = localStorage.getItem('sesionCajaId');
-      
-        const data = {
-          time_cierre: format(fechaCierre, 'HH:mm'),
-        };
-      
-        // Realizar la solicitud PUT para cerrar la caja
-        api.put(`/sesion-caja/${sesionCajaId}`, data)
-          .then(response => {
-            console.log(response.data);
-            localStorage.removeItem('sesionCajaId'); // Eliminar el ID de la sesión de caja del almacenamiento local
-            setAbrirCaja(false); // Asegurarse de que el estado de abrirCaja esté actualizado correctamente
-          })
-          .catch(error => { console.log(error) });
-      }
+    } else {
+      setMontoInicial('');
+      const fechaCierre = new Date();
+      setHoraCierre(fechaCierre.toLocaleTimeString());
+
+      const sesionCajaId = localStorage.getItem('sesionCajaId');
+
+      const data = {
+        time_cierre: format(fechaCierre, 'HH:mm'),
+      };
+
+      api.put(`/sesion-caja/${sesionCajaId}`, data)
+        .then(response => {
+          console.log(response.data);
+          setAbrirCaja(false);
+
+          api.get(`/sesion-caja/${sesionCajaId}`)
+            .then(response => {
+              console.log(response.data);
+              setMontoFinal(response.data.monto_final);
+              localStorage.removeItem('sesionCajaId'); // Eliminar la clave sesionCajaId del almacenamiento local
+              Swal.fire('Caja cerrada', `Monto Final: ${response.data.monto_final}`, 'success');
+            })
+            .catch(error => {
+              console.log(error);
+            });
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    }
   };
 
   return (
@@ -70,19 +84,26 @@ const SesionCaja = () => {
       <h1>Sesión de Caja</h1>
       <label htmlFor="montoInicial">Monto inicial:</label>
       <input
-      className='input is-primary'
+        className='input is-primary'
         type="number"
         id="montoInicial"
         value={montoInicial}
         onChange={(e) => setMontoInicial(e.target.value)}
-        disabled={abrirCaja} // Deshabilitar el campo cuando la caja esté abierta
+        disabled={abrirCaja}
         required
+        min="0"
       />
       <button className='button is-primary is-outlined' onClick={handleAbrirCerrarCaja}>
         {abrirCaja ? 'Cerrar Caja' : 'Abrir Caja'}
       </button>
       {horaCierre && (
         <p>Hora de cierre de la sesión: {horaCierre}</p>
+      )}
+      {montoFinal && (
+        <div>
+          <p>Monto Final de la sesión: {montoFinal}</p>
+          <p>Sesión de caja finalizada. La sesión ha sido eliminada.</p>
+        </div>
       )}
     </div>
   );
