ac0363243a7fb7f3ec69fd8b9cd466cd8f034440
arvidjj <andy.oj14@gmail.com>
2023-07-05 16:35:32 -0400

    minor

--- client/src/pages/SesionCaja.jsx
+++ client/src/pages/SesionCaja.jsx
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from 'react';
 import { format } from 'date-fns';
 import Swal from 'sweetalert2';
 import api from '../services/api';
-import CircularProgress from '@mui/material/CircularProgress';
 
 const formatNumberWithCommas = (number) => {
   return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
@@ -17,7 +16,6 @@ const SesionCaja = () => {
   const [montoInicial, setMontoInicial] = useState('');
   const [montoFinal, setMontoFinal] = useState('');
   const [horaCierre, setHoraCierre] = useState('');
-  const [isLoading, setLoading] = useState(false);
 
   useEffect(() => {
     const sesionCajaId = localStorage.getItem('sesionCajaId');
@@ -26,15 +24,17 @@ const SesionCaja = () => {
     }
   }, []);
 
-  const handleAbrirCerrarCaja = async () => {
-    setLoading(true);
+  const handleAbrirCerrarCaja = () => {
     if (!abrirCaja) {
       if (montoInicial === '' || Number(removeCommasFromString(montoInicial)) < 0) {
         Swal.fire('Error', 'El monto inicial no puede ser menor a 0', 'error');
         return;
       }
-      const user = JSON.parse(localStorage.getItem('user'));
+
       const fechaApertura = new Date();
+      setAbrirCaja(true);
+
+      const user = JSON.parse(localStorage.getItem('user'));
       const data = {
         id_empleado: user.id,
         id_caja: 1,
@@ -45,36 +45,37 @@ const SesionCaja = () => {
         time_cierre: format(fechaApertura, 'HH:mm'),
       };
 
-      await api.post('/sesiones-cajas', data)
+      api.post('/sesiones-cajas', data)
         .then(response => {
           console.log(response.data);
           localStorage.setItem('sesionCajaId', response.data.id);
-          Swal.fire('Caja abierta');
         })
         .catch(error => {
           console.log(error);
         });
 
-      setAbrirCaja(true);
-
     } else {
+      setMontoInicial('');
       const fechaCierre = new Date();
       setHoraCierre(fechaCierre.toLocaleTimeString());
+
+      const sesionCajaId = localStorage.getItem('sesionCajaId');
+
       const data = {
         time_cierre: format(fechaCierre, 'HH:mm'),
       };
-      const sesionCajaId = localStorage.getItem('sesionCajaId');
-      await api.put(`/sesion-caja/${sesionCajaId}`, data)
-        .then(async response => {
+
+      api.put(`/sesion-caja/${sesionCajaId}`, data)
+        .then(response => {
           console.log(response.data);
           setAbrirCaja(false);
 
-          await api.get(`/sesion-caja/${sesionCajaId}`)
+          api.get(`/sesion-caja/${sesionCajaId}`)
             .then(response => {
               console.log(response.data);
               setMontoFinal(response.data.monto_final);
               localStorage.removeItem('sesionCajaId'); // Eliminar la clave sesionCajaId del almacenamiento local
-              Swal.fire('Caja cerrada', `Monto Final: ${formatNumberWithCommas(response.data.monto_final)}`);
+              Swal.fire('Caja cerrada', `Monto Final: ${formatNumberWithCommas(response.data.monto_final)}`, 'success');
             })
             .catch(error => {
               console.log(error);
@@ -84,8 +85,6 @@ const SesionCaja = () => {
           console.log(error);
         });
     }
-    setMontoInicial('');
-    setLoading(false);
   };
 
   const handleMontoInicialChange = (e) => {
@@ -98,23 +97,31 @@ const SesionCaja = () => {
   const isAdmin = user && user.rol === 'admin';
 
   return (
-    <div className='is-flex is-flex-direction-column p-5 has-background-light column ml-auto mr-auto'
-      style={{ border: "1px solid #D4D4D4", borderRadius: "8px", maxWidth: "500px" }}>
-      <div className="is-flex is-justify-content-center is-align-items-center vh-100">
-        <div>
-          <h1 className='title'>Sesión de Caja</h1>
-          <div className='is-flex is-flex-wrap-nowrap is-align-content-stretch tags has-addons'>
-            <label className='ml-0 pl-0 pr-1 pl-1 mb-0 tag is-info is-light is-large is-size-5' htmlFor="montoInicial"
-              style={{ border: "1px solid hsl(217, 71%, 53%)", borderRight: "none" }}>Monto inicial</label>
-            <input
-              className='input is-link'
-              type="text"
-              id="montoInicial"
-              value={montoInicial}
-              onChange={handleMontoInicialChange}
-              disabled={abrirCaja}
-              required
-            />
+    <div className="is-flex is-justify-content-center is-align-items-center vh-100">
+      <div>
+        <h1 className='title'>Sesión de Caja</h1>
+        <div className='tags has-addons'>
+          <label className='tag is-info is-light is-large' htmlFor="montoInicial">Monto inicial:</label>
+          <input
+            className='tag is-large'
+            type="text"
+            id="montoInicial"
+            value={montoInicial}
+            onChange={handleMontoInicialChange}
+            disabled={abrirCaja}
+            required
+          />
+        </div>
+        <button className='button is-primary is-outlined is-small' onClick={handleAbrirCerrarCaja}>
+          {abrirCaja ? 'Cerrar Caja' : 'Abrir Caja'}
+        </button>
+        {horaCierre && (
+          <p className='subtitle'>Hora de cierre de la sesión: {horaCierre}</p>
+        )}
+        {montoFinal && (
+          <div>
+            <p className='subtitle'>Monto Final de la sesión: {formatNumberWithCommas(montoFinal)}</p>
+            <p className='subtitle'>Sesión de caja finalizada. La sesión ha sido eliminada.</p>
           </div>
         )}
         {isAdmin && (
@@ -123,7 +130,7 @@ const SesionCaja = () => {
           </button>
         )}
       </div>
-    </div >
+    </div>
   );
 };
 
